00f511b6618977dde810bd936b3db42c6e8efa13
JoshuaMaurer <maurer.joshua@gmail.com>
2024-01-29 17:12:05 -0600

    finished tutorial

--- reaction_game.rb
+++ reaction_game.rb
@@ -1,7 +1,37 @@
 require 'ruby2d'
 
-set background:'green'
+set background:'purple'
+set title: 'Reaction Game'
 
-Text.new('Click To Begin')
+message = Text.new('Click Here To Begin', x: 20, y: 20)
+game_started = false
+square = nil
+start_time = nil
+duration = nil
+
+on :mouse_down do |event|
+    puts event.x, event.y
+
+    if game_started
+        if square.contains?(event.x, event.y)
+            duration = ((Time.now - start_time) * 1000).round
+            message = Text.new("You took exactly #{duration}ms to click the square. Click to start again.", x: 20, y:20)
+            square.remove
+            game_started = false
+        end    
+    else
+        message.remove
+
+        square = Square.new(
+            x: rand(get(:width) - 20),
+            y: rand(get(:height)- 20),
+            size: 20,
+            color: 'yellow'
+        )
+
+        start_time = Time.now
+        game_started = true
+    end
+end    
 
 show
